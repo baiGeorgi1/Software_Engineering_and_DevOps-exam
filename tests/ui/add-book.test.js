4224103155f052eaa7d191b3e943f5678d8a4c5e
Georgi <gogolino1982@gmail.com>
2024-06-30 14:13:28 +0300

    UI tests Fixed

--- tests/ui/add-book.test.js
+++ tests/ui/add-book.test.js
@@ -1,8 +1,7 @@
 const { test, expect } = require('@playwright/test');
 
 test('Check add book page', async ({ page }) => {
-    await page.goto('http://localhost:8080/add-a-book');
-    const form = await page.$('list');
-    expect(form).toBeTruthy();
-  });
-  
+  await page.goto('http://localhost:8080/add-book');
+  const form = await page.$('form');
+  expect(form).toBeTruthy();
+});
